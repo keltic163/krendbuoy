f3a4868045463c2c2220d1da479706a9780c69ca
keltic163 <keltic163@gmail.com>
2026-05-01 11:49:06 +0800

    Cleanup redundant desktop-specific files and update .gitignore

--- third_party/ps12exe/src/SyntaxErrorI18nDataGetter.ps1
+++
@@ -1,8 +0,0 @@
-param ($Content,$Localize)
-
-[cultureinfo]::CurrentUICulture = $Localize
-
-$SyntaxErrors = $Tokens = $null
-$null = [System.Management.Automation.Language.Parser]::ParseInput($Content, [ref]$Tokens, [ref]$SyntaxErrors)
-
-& $PSScriptRoot/SyntaxErrorDataBuilder.ps1 $SyntaxErrors
